b44b1da80c833475e9bc26b9b5d164436ac80482
Umais <malikumais777@gmail.com>
2025-01-30 20:16:42 +0500

    add lib in updateJob

--- client/src/pages/Jobs/Modal.js
+++ client/src/pages/Jobs/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '@fortawesome/fontawesome-free/css/all.min.css';
 const ModalComponent = ({ show, onHide, onConfirm, title, message }) => {
   return (
     <div className={`modal fade ${show ? 'show' : ''}`} style={{ display: show ? 'block' : 'none' }} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
